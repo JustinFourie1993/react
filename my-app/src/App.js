bf472f2bfad2bbb8aa60a2dcd769631c0decbba0
JustinFourie1993 <>
2023-11-04 13:16:01 +0000

    add searchbar.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import LifeCyclesCWU from './components/LifeCyclesCWU';
+import SearchBar from './components/SearchBar';
+
 
 
 
@@ -8,7 +9,7 @@ import LifeCyclesCWU from './components/LifeCyclesCWU';
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCWU/>
+      <SearchBar/>
     </div>
   );
 }
---
+++ my-app/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import importedNames from '../names'
+
+export class SearchBar extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         names: importedNames
+      }
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Name Search</h1>
+            <p>matching names found: {this.state.names.length}</p>
+            <form>
+                <input type="text" placeholder="serch names..."/>
+            </form>
+            {this.state.names.map(name =>{
+                return<p key={name}>{name}</p>
+            })}
+            <div>we will render names here</div>
+        </div>
+        )
+    }
+}
+
+export default SearchBar
