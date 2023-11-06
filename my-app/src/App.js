6c3705a60574c91045e726586d7fb736f17591f9
JustinFourie1993 <>
2023-11-06 13:43:55 +0000

    add hooks components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
-
-
 
 
 
 function App() {
   return (
     <div className="App">
-      <SearchBar/>
+      
     </div>
   );
 }
---
+++ my-app/src/components/UseEffectCounter.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+
+function UseEffectCounter() {
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        console.log('count 1 effect');
+        document.title = count;
+    }, [count]);
+    useEffect(() => {
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1);
+        }, 1000);
+
+        // Cleanup function to clear the interval
+        return () => {
+            clearInterval(interval);
+            console.log('Clearing timer');
+        };
+    }, []); // This array means the effect runs once on mount and cleanup on unmount
+
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
+}
+
+export default UseEffectCounter
